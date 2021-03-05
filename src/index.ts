211cb2b083e940eb14216087f3bc0175cbfeec32
Chris Rebbelin <cr.rebbelin@gmail.com>
2021-03-05 21:06:11 +0100

    Add websockets

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
 import * as express from "express";
+import { Server as WebsocketServer } from 'ws';
 import { join, resolve } from "path";
 
 const port = process.env.PORT || 3000;
 
-express()
+const app = express()
   .use(express.static(join(__dirname, "static")))
+  .post('/load', (req, res) => {
+    
+  })
   .listen(port, () => console.log(`Server is listening on port ${port}.`));
+
+
+
+const wss = new WebsocketServer({ server: app });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 2000);
--- static/index.js
+++ static/index.js
@@ -1,3 +1,11 @@
-document.querySelector('button').addEventListener('click', () => {
+const HOST = location.origin.replace(/^http/, 'ws');
+const ws = new WebSocket(HOST);
+const result = document.querySelector('.wss');
+
+ws.onmessage = (event) => {
+  result.textContent = 'Server time: ' + event.data;
+};
+
+document.querySelector('button').addEventListener('click', async () => {
     alert('clicked!');
 });
