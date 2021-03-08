358564cd5dfd04559d863279e79ad54585de1ea5
Chris Rebbelin <cr.rebbelin@gmail.com>
2021-03-08 20:43:52 +0100

    Remove preview

--- static/client.js
+++ static/client.js
@@ -6,33 +6,27 @@ const input = document.querySelector('input');
 
 const loadBtn = document.getElementById('load-btn');
 const downloader = document.getElementById('dl-btn');
-const previewer = document.getElementById('preview-btn');
-
-const actions = document.querySelector('.actions');
 
 let ourl = null;
 
-ws.onmessage = ({ data }) => {
+ws.addEventListener('message', ({ data }) => {
   if ('string' === typeof data) {
+
     const txt = document.createElement('p');
     txt.textContent = data;
     logger.appendChild(txt);
-    return;
-  }
 
-  if (data instanceof ArrayBuffer) {
+  } else if (data instanceof ArrayBuffer) {
+
     const blob = new Blob([data], { type: 'application/pdf' });
     ourl = URL.createObjectURL(blob);
 
     downloader.setAttribute('href', ourl);
     downloader.setAttribute('download', 'Sheet.pdf');
 
-    previewer.setAttribute('href', ourl);
-
-    actions.classList.add('shown');
-    return;
+    downloader.classList.add('shown');
   }
-};
+});
 
 loadBtn.addEventListener('click', () => {
   if (ws.readyState !== ws.OPEN) {
@@ -46,12 +40,10 @@ loadBtn.addEventListener('click', () => {
     downloader.removeAttribute('href');
     downloader.removeAttribute('download');
 
-    previewer.removeAttribute('href');
-
     URL.revokeObjectURL(ourl);
     ourl = null;
   }
   logger.innerHTML = '';
-  actions.classList.remove('shown');
+  downloader.classList.remove('shown');
   ws.send(url);
 });
