f2b5008fef052a4b1ef2da995f9f937f51bafa35
Chris Rebbelin <cr.rebbelin@gmail.com>
2021-03-15 14:05:34 +0100

    Wrap client logic in IIFE

--- static/client.js
+++ static/client.js
@@ -1,55 +1,52 @@
-const ws = new WebSocket(location.origin.replace(/^http/, 'ws'));
-ws.binaryType = 'arraybuffer';
+(() => {
+  const ws = new WebSocket(location.origin.replace(/^http/, 'ws'));
+  ws.binaryType = 'arraybuffer';
 
-const logger = document.querySelector('.logs');
-const input = document.querySelector('input');
+  const logger = document.querySelector('.logs');
+  const input = document.querySelector('input');
 
-const loadBtn = document.getElementById('load-btn');
-const downloader = document.getElementById('dl-btn');
+  const loadBtn = document.getElementById('load-btn');
+  const downloader = document.getElementById('dl-btn');
 
-let ourl = null;
+  let ourl = null;
 
-ws.addEventListener('message', ({ data }) => {
+  ws.addEventListener('message', ({ data }) => {
+    if ('string' === typeof data) {
+      const txt = document.createElement('p');
+      txt.textContent = data;
+      logger.appendChild(txt);
+    } else if (data instanceof ArrayBuffer) {
+      const blob = new Blob([data], { type: 'application/pdf' });
+      ourl = URL.createObjectURL(blob);
 
-  if ('string' === typeof data) {
+      downloader.setAttribute('href', ourl);
+      downloader.setAttribute('download', 'Sheet.pdf');
 
-    const txt = document.createElement('p');
-    txt.textContent = data;
-    logger.appendChild(txt);
+      downloader.classList.add('shown');
+      loadBtn.removeAttribute('disabled');
+    }
+  });
 
-  } else if (data instanceof ArrayBuffer) {
+  loadBtn.addEventListener('click', () => {
+    if (ws.readyState !== ws.OPEN) {
+      alert('Disconnected, Reload the page to fix!');
+      return;
+    }
+    const url = input.value.trim();
+    if (!url) return;
 
-    const blob = new Blob([data], { type: 'application/pdf' });
-    ourl = URL.createObjectURL(blob);
+    if (ourl) {
+      downloader.removeAttribute('href');
+      downloader.removeAttribute('download');
 
-    downloader.setAttribute('href', ourl);
-    downloader.setAttribute('download', 'Sheet.pdf');
+      URL.revokeObjectURL(ourl);
+      ourl = null;
+    }
 
-    downloader.classList.add('shown');
-    loadBtn.removeAttribute('disabled');
-  }
-});
+    logger.innerHTML = '';
+    downloader.classList.remove('shown');
+    loadBtn.setAttribute('disabled', 'disabled');
 
-loadBtn.addEventListener('click', () => {
-
-  if (ws.readyState !== ws.OPEN) {
-    alert('Disconnected, Reload the page to fix!');
-    return;
-  }
-  const url = input.value.trim();
-  if (!url) return;
-
-  if (ourl) {
-    downloader.removeAttribute('href');
-    downloader.removeAttribute('download');
-
-    URL.revokeObjectURL(ourl);
-    ourl = null;
-  }
-
-  logger.innerHTML = '';
-  downloader.classList.remove('shown');
-  loadBtn.setAttribute('disabled', 'disabled');
-
-  ws.send(url);
-});
+    ws.send(url);
+  });
+})();
